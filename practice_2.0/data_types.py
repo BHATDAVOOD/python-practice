5f9125c4cd2d7c07ba419e8c1f67ab111b730ab9
Davood Ahmad Bhat(UST <257642@ust.com>
2025-05-13 10:42:16 +0530

    Python Numeric Data types

---
+++ practice_2.0/data_types.py
@@ -0,0 +1,13 @@
+# Python Numeric Data Types
+# int, bool, float, complex
+# complex - made of 2 parts - real & imaginary separated by + or - sign. Imaginary is suffixed by 'j'
+
+complexType=4+5j
+print(complexType)
+print(type(complexType))
+
+a,b,c,d = 10,20.7,False,10+3j
+print("The type of variable having value ",a," is ",type(a))
+print("The type of variable having value ",b," is ",type(b))
+print("The type of variable having value ",c," is ",type(c))
+print("The type of variable having value ",d," is ",type(d))
