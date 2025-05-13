1f216060dc201d8cb61d6ea1e5c010b054d0abc2
Davood Ahmad Bhat(UST <257642@ust.com>
2025-05-13 10:49:28 +0530

    String Data types

--- practice_2.0/data_types.py
+++ practice_2.0/data_types.py
@@ -11,3 +11,21 @@ print("The type of variable having value ",a," is ",type(a))
 print("The type of variable having value ",b," is ",type(b))
 print("The type of variable having value ",c," is ",type(c))
 print("The type of variable having value ",d," is ",type(d))
+
+# Python String Data Type
+# Enclosed in Single,Double or Triple Quotation marks
+
+firstName='davood'
+middleName="ahmad"
+lastName='''bhat'''
+
+print(firstName,middleName,lastName)
+print(type(firstName),type(middleName),type(lastName))
+
+# Arithmetic operations can not be performed on String Data type
+# But Slicing and Concatenation can be performed
+
+print(firstName[0]) # Prints first character
+print(firstName[2:4]) # Prints from 2nd Index till 3rd Index 
+print(firstName[2:]) # 2nd Index till end
+print(firstName[:3]) # Starting to 2nd Index
